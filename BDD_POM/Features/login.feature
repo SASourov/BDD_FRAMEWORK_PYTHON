765e44daf07ee65e0f0d90090a89bebe8c841a63
shakil.ahmed <shakil.ahmed@amiprobashi.com>
2024-04-28 20:48:12 +0600

    Add some test step

--- BDD_POM/Features/login.feature
+++ BDD_POM/Features/login.feature
@@ -1,8 +1,16 @@
 Feature: Test Feature
 
+  @test1
   Scenario: Login Functionality
     Given user open web site
     When user click login menu
     And user entre username and email address
     And user click login button
-    Then user get success message
+#    And user get success message
+    And user select gender
+    And user set password
+    And user set birth info
+    And user select an option
+    And user set name info
+    And user set address info
+    And user click button
--- BDD_POM/Steps/loginStep.py
+++ BDD_POM/Steps/loginStep.py
@@ -1,8 +1,11 @@
+import time
+
 from behave import *
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 from BDD_POM.Steps.loginPage import Login
+from BDD_POM.Steps.accountInfoPage import AccountInfo
 
 
 @given('user open web site')
@@ -22,7 +25,7 @@ def click_login_menu(context):
 @when("user entre username and email address")
 def set_username_and_email(context):
     context.login.set_username("Shakil")
-    context.login.set_user_email("shakil@mail.com")
+    context.login.set_user_email("shakil001@mail.com")
 
 
 @when("user click login button")
@@ -30,14 +33,70 @@ def click_login_button(context):
     context.login.click_login_button()
 
 
-@then("user get success message")
-def get_success_message(context):
-    message = context.driver.find_element(By.XPATH, "//h2[@class='title text-center']//b").text
+# @when("user get success message")
+# def get_success_message(context):
+#     message = context.driver.find_element(By.XPATH, "//h2[@class='title text-center']//b").text
+#
+#     if message == "ENTER ACCOUNT INFORMATION":
+#         assert True
+#
+#     else:
+#         assert False
 
-    if message == "ENTER ACCOUNT INFORMATION":
-        assert True
 
-    else:
-        assert False
+@when('user select gender')
+def select_gender(context):
+    # context.driver = webdriver.Chrome()
+    # context.driver.maximize_window()
+    # context.driver.implicitly_wait(10)
+    # context.driver.get("https://automationexercise.com/")
+    context.ac_info = AccountInfo(context.driver)
+    context.ac_info.click_select_gender()
+    time.sleep(2)
 
 
+@when('user set password')
+def user_set_password(context):
+    context.ac_info.set_password('Test@12')
+    time.sleep(2)
+
+
+@when('user set birth info')
+def user_set_birth_info(context):
+    context.ac_info.click_on_birth_date()
+    context.ac_info.click_on_birth_month()
+    context.ac_info.click_on_birth_year()
+    time.sleep(2)
+
+
+@when('user select an option')
+def user_select_an_option(context):
+    context.ac_info.select_option()
+    time.sleep(2)
+
+
+@when('user set name info')
+def user_set_name_info(context):
+    context.ac_info.set_first_name("Abul")
+    context.ac_info.set_last_name("Kalam")
+    time.sleep(2)
+
+
+@when('user set address info')
+def user_set_address_info(context):
+    context.ac_info.set_address("Banani Dhaka")
+    time.sleep(2)
+    context.ac_info.set_country()
+    time.sleep(2)
+    context.ac_info.set_state("Capital")
+    time.sleep(2)
+    context.ac_info.set_city("City")
+    time.sleep(2)
+    context.ac_info.set_zip("1230")
+    time.sleep(2)
+    context.ac_info.set_mobile_num("01722000000")
+
+
+@when('user click button')
+def user_click_button(context):
+    context.ac_info.click_on_create_button()
